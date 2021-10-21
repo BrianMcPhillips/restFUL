62f476eceddc3d22dc163ad5a2006a32142c7841
Brian McPhillips <B.McPhillips89@gmail.com>
2021-10-20 23:47:23 -0700

    Handle State functions for form

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 export default class Search extends Component {
+  state = {
+    urlInput: '',
+    radioInput: 'GET',
+    bodyInput: '',
+
+  }
+  
+  handleUrlInput = ({ target }) => {
+    this.setState({ urlInput: target.value });
+  } 
+
+  handleRadioInput = ({ target }) => {
+    this.setState({ radioInput: target.value });
+  }
+
+  bodyInput = ({ target }) => {
+    this.setState({ bodyInput: target.value });
+  }
+  
   render() {
     return (
       <div>
